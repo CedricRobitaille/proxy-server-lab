4fbc49c65286379d0ae75529afdfd00ac69d27dd
Cedric <crubbertire@rogers.com>
2025-11-04 15:29:38 -0500

    Set up fetch

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.get("/", async (req, res) => {
+  const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${req.query.city}`)
+  const data = await response.json();
+  console.log(data);
+  res.json(data)
+})
+
 app.listen(3000, () => {
-  console.log("listening on port 3000")
+  console.log("listening on port 3000");
 })
