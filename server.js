13afdad09425a52d9357165c0644383c708fee53
Cedric <crubbertire@rogers.com>
2025-11-04 15:35:16 -0500

    Set up proxy fetch for Weather App

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
+
+const env = require("dotenv");
+env.config()
 
 app.get("/", async (req, res) => {
-  const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${req.query.city}`)
-  const data = await response.json();
+  const apiResponse = await fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${req.query.city}`)
+  const data = await apiResponse.json();
   console.log(data);
   res.json(data)
 })
